5270442b55bfe9abc248536767431b5afdb214c1
Arhipov.OM <arhipov.om@mail.ru>
2025-09-02 16:28:47 +0300

    perf(telegram): кэширование списка напоминаний в manage_reminder
    Добавлено кэширование списка напоминаний в dialog_data для предотвращения повторных запросов к сервису при получении списка напоминаний в диалоге управления. Теперь данные запрашиваются только если они отсутствуют в dialog_data.

--- headway/presentation/telegram/dialogs/manage_reminder/getters.py
+++ headway/presentation/telegram/dialogs/manage_reminder/getters.py
@@ -6,14 +6,16 @@ from headway.application.services import ReminderService, UserService
 
 
 async def reminders_list(dialog_manager: DialogManager, event_update: Update, **_):
-    user_service: UserService = dialog_manager.middleware_data.get('user_service')
-    reminder_service: ReminderService = dialog_manager.middleware_data.get('reminder_service')
+    if not dialog_manager.dialog_data.get('reminders'):
+        user_service: UserService = dialog_manager.middleware_data.get('user_service')
+        reminder_service: ReminderService = dialog_manager.middleware_data.get('reminder_service')
 
-    user = await user_service.get_user_by_identity(provider_id=str(event_update.callback_query.from_user.id))
-    data = await reminder_service.list_reminders_by_user(user_id=user.id)
-    return {
-        "reminders": data
-    }
+        user = await user_service.get_user_by_identity(provider_id=str(event_update.callback_query.from_user.id))
+        data = await reminder_service.list_reminders_by_user(user_id=user.id)
+        dialog_manager.dialog_data['reminders'] = data
+    else:
+        data = dialog_manager.dialog_data.get('reminders')
+    return {"reminders": data}
 
 
 async def get_reminder(dialog_manager: DialogManager, event_update: Update, **_) -> dict[str, ReminderDTO] | None:
